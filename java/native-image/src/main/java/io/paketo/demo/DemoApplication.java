fc68ab8d98e89be6cce29b57ca556449315e9c3a
Ben Hale <bhale@vmware.com>
2020-11-13 10:17:32 -0800

    Application Update
    This change updates the application for the latest releases of the native
    image functionality.
    
    Signed-off-by: Ben Hale <bhale@vmware.com>

--- java/native-image/src/main/java/io/paketo/demo/DemoApplication.java
+++ java/native-image/src/main/java/io/paketo/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package io.paketo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(proxyBeanMethods=false)
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- java/native-image/src/test/java/io/paketo/demo/DemoApplicationTests.java
+++ java/native-image/src/test/java/io/paketo/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package io.paketo.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
