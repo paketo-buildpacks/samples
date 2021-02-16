d5348d0af3b20ff37ad83d182894ad14548e9dc2
Forest Eckhardt <feckhardt@pivotal.io>
2021-02-16 16:01:44 +0000

    Adds sample Go app that does not import modules
    - Adds a sample that does not import any modules to demonstrate that
    workflow which is supported by the Go buildpack
    - Adds this sample to the tests

--- tests/go_test.go
+++ tests/go_test.go
@@ -50,6 +50,36 @@ func testGoWithBuilder(builder string) func(*testing.T, spec.G, spec.S) {
 				Expect(os.RemoveAll(source)).To(Succeed())
 			})
 
+			context("no imports", func() {
+				it("builds successfully", func() {
+					var err error
+					source, err = occam.Source(filepath.Join("../go", "no-imports"))
+					Expect(err).NotTo(HaveOccurred())
+
+					var logs fmt.Stringer
+					image, logs, err = pack.Build.
+						WithPullPolicy("never").
+						WithBuilder(builder).
+						Execute(name, source)
+					Expect(err).ToNot(HaveOccurred(), logs.String)
+
+					Expect(logs).To(ContainLines(ContainSubstring("Paketo Go Distribution Buildpack")))
+					Expect(logs).To(ContainLines(ContainSubstring("Paketo Go Build Buildpack")))
+
+					Expect(logs).NotTo(ContainLines(ContainSubstring("Paketo Go Mod Vendor Buildpack")))
+					Expect(logs).NotTo(ContainLines(ContainSubstring("Paketo Dep Buildpack")))
+					Expect(logs).NotTo(ContainLines(ContainSubstring("Paketo Dep Ensure Buildpack")))
+
+					container, err = docker.Container.Run.
+						WithEnv(map[string]string{"PORT": "8080"}).
+						WithPublish("8080").
+						Execute(image.ID)
+					Expect(err).NotTo(HaveOccurred())
+
+					Eventually(container).Should(Serve(ContainSubstring("Powered By Paketo Buildpacks")).OnPort(8080))
+				})
+			})
+
 			context("uses go modules", func() {
 				it("builds successfully", func() {
 					var err error
