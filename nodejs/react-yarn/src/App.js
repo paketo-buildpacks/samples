f49c1e004aadd3649d69d9d2cf5351ca7b5b563d
Bryan Huynh <bryanh@vmware.com>
2021-08-16 11:45:04 -0500

    Update react-yarn sample

--- nodejs/react-yarn/src/App.js
+++ nodejs/react-yarn/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Powered by Paketo Buildpacks</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://paketo.io/images/paketo-logo-full-color.png" className="App-logo" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
