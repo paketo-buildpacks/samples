e5dd7f15faf7a0ea2540964fa878cdbdbad35339
Eddú Meléndez <eddu.melendez@gmail.com>
2021-08-16 16:32:46 -0500

    Add :main

--- java/tools-build/build.clj
+++ java/tools-build/build.clj
@@ -27,4 +27,5 @@
                       :class-dir class-dir})
       (b/uber {:class-dir class-dir
                :uber-file uber-file
-               :basis basis}))
+               :basis basis
+               :main  "toolsbuild_source.server"}))
