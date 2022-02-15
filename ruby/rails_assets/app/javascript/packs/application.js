42e81bf6962a8d2a52c92af0e2948649ec7ce870
Sophie Wigmore <swigmore@vmware.com>
2022-02-15 09:34:38 -0500

    add rails sample app

---
+++ ruby/rails_assets/app/javascript/packs/application.js
@@ -0,0 +1,22 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+window.onload = function(event) {
+  var div = document.createElement('div');
+  div.innerText = "And hello from Javascript!";
+
+  var body = document.querySelector('body');
+  body.appendChild(div);
+};
