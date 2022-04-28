d247b1da0de45a18d800412b7ebb4446d5af60ba
Daniel Mikusa <dmikusa@vmware.com>
2022-04-28 09:37:07 -0400

    Update to Spring Boot 2.6.7, as well as some other updates (#319)
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>
    
    Co-authored-by: Forest Eckhardt <feckhardt@pivotal.io>

--- java/kotlin/build.gradle.kts
+++ java/kotlin/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.12"
+	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.32"
-  	kotlin("plugin.spring") version "1.5.32"
+	kotlin("jvm") version "1.6.21"
+  	kotlin("plugin.spring") version "1.6.21"
 }
 
 group = "io.paketo"
--- java/leiningen/project.clj
+++ java/leiningen/project.clj
@@ -1,11 +1,11 @@
 (defproject demo "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [io.pedestal/pedestal.service "0.5.10"]
                  [io.pedestal/pedestal.jetty "0.5.10"]
-                 [ch.qos.logback/logback-classic "1.2.10" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.33"]
-                 [org.slf4j/jcl-over-slf4j "1.7.33"]
-                 [org.slf4j/log4j-over-slf4j "1.7.33"]]
+                 [ch.qos.logback/logback-classic "1.2.11" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.36"]
+                 [org.slf4j/jcl-over-slf4j "1.7.36"]
+                 [org.slf4j/log4j-over-slf4j "1.7.36"]]
   :resource-paths ["config"]
   :main ^:skip-aot lein-source.server
   :profiles {:uberjar {:aot :all
