07006e4aad20fee48f7502c386331fa3b7fe74a2
Daniel Mikusa <dmikusa@vmware.com>
2022-06-29 15:00:20 -0400

    Bump Spring from 2.7.0 -> 2.7.1 in all projects (#356)
    Signed-off-by: Daniel Mikusa <dmikusa@vmware.com>

--- java/kotlin/build.gradle.kts
+++ java/kotlin/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.0"
+	id("org.springframework.boot") version "2.7.1"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
   	kotlin("plugin.spring") version "1.6.21"
