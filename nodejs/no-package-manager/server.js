ce3575c68d41a57a2e77638a84d53e39d6cbafa1
Costas Papastathis <papastathiscr@gmail.com>
2024-07-12 15:21:16 +0300

    adding health check route (#749)

--- nodejs/no-package-manager/server.js
+++ nodejs/no-package-manager/server.js
@@ -2,6 +2,12 @@ const http = require('http')
 const port = process.env.PORT || 8080
 
 const requestHandler = (request, response) => {
+
+  const requestTime = new Date(Date.now()).toString();
+  console.log(request.method, request.headers.host, request.url, requestTime);
+
+  if (request.url === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
     response.end(`<!DOCTYPE html>
 <html>
   <head>
@@ -10,8 +16,18 @@ const requestHandler = (request, response) => {
   <body>
     <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
   </body>
-</html>`)
-}
+</html>`);
+  } else if (
+    request.url === "/actuator/health" &&
+    request.method === "GET"
+  ) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ status: "UP" }));
+  } else {
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.end("Not Found");
+  }
+};
 
 const server = http.createServer(requestHandler)
 
