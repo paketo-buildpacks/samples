bb757d50891ca4aea1d892072b00bee6ca71a80d
Daniel Mikusa <dan@mikusa.com>
2024-08-26 09:36:30 -0400

    Add simple WAR test that does not use Spring
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

---
+++ java/war/src/main/java/io/paketo/samples/ActuatorServlet.java
@@ -0,0 +1,19 @@
+package io.openliberty.guides.hello;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/actuator/health")
+public class ActuatorServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        response.getWriter().append("{\"status\":\"UP\"}\n");
+    }
+}
